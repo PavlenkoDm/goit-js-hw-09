3309f16de3c95bd15d4ee54d97de98a3efaf75ec
PavlenkoDm <dmpavlenko07@gmail.com>
2023-02-25 23:51:18 +0300

    Update 3 promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,4 @@
 
-let firstDelay = null;
-let amount = null;
 let delayStep = null;
 
 const refs = {
@@ -11,16 +9,24 @@ refs.form.addEventListener('submit', onSubmit)
 
 
 
+
 function onSubmit(event) {
   event.preventDefault();
-  firstDelay = refs.form.elements.delay.value;
+  let firstDelay = refs.form.elements.delay.value;
   delayStep = refs.form.elements.step.value;
-  amount = refs.form.elements.amount.value;
+  const amount = refs.form.elements.amount.value;
    
   if (amount <= 0) return;
 
   for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, firstDelay);
+    
+    createPromise(i, firstDelay)
+      .then(({ pos, del }) => {
+        console.log(`Fulfilled promise`);
+      })
+      .catch(({ pos, del }) => {
+        console.log(`Rejected promise`);
+      });
     firstDelay += delayStep;
   }
 }
@@ -28,9 +34,15 @@ function onSubmit(event) {
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+          reject({position, delay});
+        }
+    }, delay);
+  });
+  
 }
